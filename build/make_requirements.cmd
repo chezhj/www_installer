e654d028fef638e92ec7445079ec3266fa5a80b4
Hendrik-Jan <h@vdwaal.net>
2026-01-23 21:32:08 +0100

    fix: CHanged cd to reflect path

--- build/make_requirements.cmd
+++ build/make_requirements.cmd
@@ -1,6 +1,6 @@
 @echo off
 REM Change directory to project root (parent of this script's directory)
-cd /d "%~dp0.."
+cd /d "%~dp0..\.."
 
 echo Exporting requirements to requirements.txt...
 poetry export -f requirements.txt --output requirements.txt --without-hashes
--- build/push_all.cmd
+++ build/push_all.cmd
@@ -2,7 +2,7 @@ git push origin --tags
 
 @echo off
 REM Navigate to the project root (parent of the build directory)
-cd /d "%~dp0.."
+cd /d "%~dp0..\.."
 
 echo Pushing changes and tags to remote...
 REM --follow-tags pushes the commit and any tags attached to it (created by commitizen)
