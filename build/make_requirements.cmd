98db719ffd4d022f43a58dfeba380a62f8261fcd
Hendrik-Jan <h@vdwaal.net>
2026-02-07 19:22:22 +0100

    feat: Added test tun to make_requirements

--- build/make_requirements.cmd
+++ build/make_requirements.cmd
@@ -2,5 +2,20 @@
 REM Change directory to project root (parent of this script's directory)
 cd /d "%~dp0..\.."
 
+
 echo Exporting requirements to requirements.txt...
 poetry export -f requirements.txt --output requirements.txt --without-hashes
+
+
+set SETTINGS_MODULE=%1
+if not "%SETTINGS_MODULE%"=="" (
+    echo Running Django tests with %SETTINGS_MODULE%...
+    python manage.py test --settings=%SETTINGS_MODULE%
+    if %ERRORLEVEL% NEQ 0 (
+        echo Tests failed! Aborting bump.
+        exit /b 1
+    )
+)
+
+echo All pre-bump checks passed!
+exit /b 0
